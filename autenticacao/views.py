75032e6b5c09787132217dc3bdb0aa8d14032ee0
EnderUser <enderuser@outlook.com>
2023-10-15 17:11:48 -0300

    atualizado app de autenticacao user

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import re
+from .utils import password_is_valid
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -13,7 +15,24 @@ def cadastro(request):
         email = request.POST.get('email')
         confirmar_senha = request.POST.get('confirmar_senha')
         
-        return HttpResponse(confirmar_senha)
+        if not password_is_valid(request, senha, confirmar_senha):
+            return redirect('/auth/cadastro')
+        
+        try:
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=senha,
+                                            is_active=False)
+            user.save()
+            messages.add_message(request, constants.SUCCESS, 'Usuario Cadastrado com Sucesso')
+            return redirect('/auth/logar')
+        except:
+            messages.add_message(request, constants.ERROR, 'Erro interno do Sistema')
+            return redirect('/auth/cadastro')
 
 def logar(request):
-    pass
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('usuario')
+        senha = request.POST.get('senha')
--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from django.contrib.messages import constants
 import os
 from dotenv import load_dotenv
 
@@ -123,3 +124,12 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Messages TAGS
+MESSAGE_TAGS = {
+    constants.DEBUG: 'alert-primary',
+    constants.ERROR: 'alert-danger',
+    constants.SUCCESS: 'alert-success',
+    constants.INFO: 'alert-info',
+    constants.WARNING: 'alert-warning',
+}
